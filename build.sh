1c3737a68100a0ff9082c5a61e38ec98d63a0828
Piotr Kabaciński <piotr.kabacinski@codequest.com>
2023-05-19 12:52:10 +0200

    feat: Update tsconfig

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 OUT_DIR="dist"
 
-./node_modules/.bin/esbuild src/*.ts --bundle --minify --outdir=$OUT_DIR && \
-./node_modules/.bin/esbuild src/*.css --bundle --minify --outdir=$OUT_DIR && \
+./node_modules/.bin/esbuild src/*.ts src/*.css --bundle --minify --outdir=$OUT_DIR && \
 find ./src -type f \( -name "*.html" -o -name "*.json" \) -exec cp {} ./$OUT_DIR \;
