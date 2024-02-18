f9cbd03794ca2be7fd3a7fdb3c95961209ebb900
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2024-02-18 13:09:35 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
-OUT_DIR="dist"
-
 ./node_modules/.bin/esbuild src/*.ts src/*.css --bundle --minify --outdir=$OUT_DIR && \
 find ./src -type f \( -name "*.html" -o -name "*.json" -o -name "*.mustache" \) -exec cp {} ./$OUT_DIR \;
