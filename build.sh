2b96da093d1d93be2ccc66a9e10048bc201119d4
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2024-06-07 17:52:45 +0200

    feat: Add extension icons

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 rm -rf $OUT_DIR && \
+# Build & minify script files
 ./node_modules/.bin/esbuild \
 src/panel/panel.ts \
 src/panel/css/panel.css \
@@ -6,4 +7,7 @@ src/settings/settings.ts \
 src/settings/css/settings.css \
 src/*.ts \
 --bundle --minify --outdir=$OUT_DIR && \
+# Copy icons directory
+cp -r ./src/icons ./$OUT_DIR && \
+# Copy assets
 find ./src -type f \( -name "*.html" -o -name "*.json" -o -name "*.mustache" \) -exec cp {} ./$OUT_DIR \;
